ef65ad0173cefc7b3532e046b10a1e13add0445e
NaGiBaToR9000 <wpusti@yandex.ru>
2022-05-09 15:07:31 +0300

    dnt wrk

--- BenchmarkProject/BenchmarkProject/Program.cs
+++ BenchmarkProject/BenchmarkProject/Program.cs
@@ -12,104 +12,33 @@ namespace BenchmarkProject
     {
         static void Main(string[] args)
         {
-            BenchmarkRunner.Run<BenchmarkClass>();
+            BenchmarkRunner.Run(typeof(Program).Assembly);
+            //BenchmarkSwitcher.FromAssembly(typeof(Program).Assembly).Run(args);
         }
     }
     //[MemoryDiagnoser]
-    //[RankColumn]
-    public class BenchmarkClass
+    [RankColumn]
+    public class BechmarkClass
     {
-        PointClass pointClass1 = new PointClass(8, 15);
-        PointClass pointClass2 = new PointClass(1, 22);
-        PointStruct pointStruct1 = new PointStruct(1, 9);
-        PointStruct pointStruct2 = new PointStruct(32, 4);
-
-        [Benchmark]
-        void Test1()
+        public int SumValueType(int value)
         {
-            DistanceCalculation.DistancePointClassFloat(pointClass1, pointClass2);
+            return 9 + value;
         }
-
-        [Benchmark]
-        void Test2()
+        public int SumRefType(object value)
         {
-            DistanceCalculation.DistancePointStructFloat(pointStruct1, pointStruct2);
+            return 9 + (int)value;
         }
-
         [Benchmark]
-        void Test3()
+        public void TestSum()
         {
-            DistanceCalculation.DistancePointStructDouble(pointStruct1, pointStruct2);
+            SumValueType(99);
         }
-
         [Benchmark]
-        void Test4()
+        public void TestSumBoxing()
         {
-            DistanceCalculation.DistancePointStructFloatWithoutSquareRoot(pointStruct1, pointStruct2);
+            object x = 99;
+            SumRefType(x);
         }
     }
 
-    public static class DistanceCalculation
-    {
-        /// <summary>
-        /// 1) Обычный метод расчёта дистанции со ссылочным типом (PointClass — координаты типа float)
-        /// </summary>
-        public static float DistancePointClassFloat(PointClass point1, PointClass point2)
-        {
-            float x = point1.x - point2.x;
-            float y = point1.y - point2.y;
-            return (float)Math.Sqrt((x * x) + (y * y));
-        }
-
-        /// <summary>
-        /// 2) Обычный метод расчёта дистанции со значимым типом (PointStruct — координаты типа float).
-        /// </summary>
-        public static float DistancePointStructFloat(PointStruct point1, PointStruct point2)
-        {
-            float x = point1.x - point2.x;
-            float y = point1.y - point2.y;
-            return (float)Math.Sqrt((x * x) + (y * y));
-        }
-
-        /// <summary>
-        /// 3) Обычный метод расчёта дистанции со значимым типом (PointStruct — координаты типа double).
-        /// </summary>
-        public static double DistancePointStructDouble(PointStruct point1, PointStruct point2)
-        {
-            double x = point1.x - point2.x;
-            double y = point1.y - point2.y;
-            return Math.Sqrt((x * x) + (y * y));
-        }
-
-        /// <summary>
-        /// Метод расчёта дистанции без квадратного корня со значимым типом (PointStruct — координаты типа float).
-        /// </summary>
-        public static float DistancePointStructFloatWithoutSquareRoot(PointStruct point1, PointStruct point2)
-        {
-            float x = point1.x - point2.x;
-            float y = point1.y - point2.y;
-            return (x * x + y * y);
-        }
-    }
-
-    public struct PointStruct
-    {
-        public int x;
-        public int y;
-        public PointStruct(int value1, int value2)
-        {
-            x = value1;
-            y = value2;
-        }
-    }
-    public class PointClass
-    {
-        public int x;
-        public int y;
-        public PointClass(int value1, int value2)
-        {
-            x = value1;
-            y = value2;
-        }
-    }
 }
