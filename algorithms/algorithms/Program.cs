46e4c75e47292e5d42af4fc378fae36dc31c87d3
NaGiBaToR9000 <wpusti@yandex.ru>
2022-05-26 22:53:24 +0300

    create homework

--- algorithms/algorithms/Program.cs
+++ algorithms/algorithms/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Реализовать Bucketsort, проверить корректность работы.
+public static class Sort
+{
+    public static int[] Busket(int[] values)
+    {
+        // создание контейнеров
+        int size = 10;
+        int[][] buskets = new int[5][];
+        for (int i = 0; i < buskets.Length; i++)
+            
+
+        // раскидать элементы по блокам
+        foreach(int value in values)
+        {
+
+        }
+
+        return values;
+    }
+}
